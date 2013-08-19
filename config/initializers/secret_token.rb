513519c9255214c30d64863e65af413aae786e8b
vtl-pol <vitaliypol1992@gmail.com>
2013-08-19 17:47:36 +0300

    divshot main page clone

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BootstrapExample::Application.config.secret_key_base = 'c8a7a887bbeb53221ad3013ac831ae291002d430e15c9f40ab0cfdc8f14c570538c7555fa6052472fcd35af04b22f4bd5d3ebea2b5e8bbdbb1e0689b612270f0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BootstrapExample::Application.config.session_store :cookie_store, key: '_bootstrap_example_session'
