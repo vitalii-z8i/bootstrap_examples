cd0dab8a4a7b1620f4076564b8f57681bc1984a6
vtl-pol <vitaliypol1992@gmail.com>
2013-08-20 14:52:35 +0300

    Animated twitter panel. Playable video

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,61 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require video
+//= require video.dev
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+
+  $("#divshot-screencast").length&&(
+    videojs.options.flash.swf="http://divshot.com/assets/video-js.swf",
+    s=videojs("divshot-screencast",{},function(){
+      return vjs("divshot-screencast").ready(function(){
+        var t;
+        return s=this,this.player_.tech.el_.playbackRate=10, 
+               t=this.controlBar,
+               t.fadeOut(),
+               s.player_.on("timeupdate",function(){
+                return $(".screencast-info .time").html(this.currentTime())
+               })
+      })
+    }),
+    $(".screencast-info .controls a").click(function(t){
+      var e;
+      switch(
+        s=vjs("divshot-screencast"),
+        $(".screencast-info .controls a.active").removeClass("active"),
+        $(this).addClass("active"),
+        e=$(this).data("action")
+      )
+      {
+        case"pause":s.pause(),
+          s.controlBar.fadeIn(),
+          $(".hero-actions").show();
+          break;  
+        case"play":s.play(),
+          setTimeout(function(){
+            return s.player_.tech.el_.playbackRate=1
+          },100),
+          s.controlBar.fadeOut(),
+          $(".hero-actions").hide();
+          break;
+        case"play-2x":s.play(),
+          setTimeout(function(){
+            return s.player_.tech.el_.playbackRate=10
+          },100),
+          s.controlBar.fadeOut(),
+          $(".hero-actions").hide();
+          break;
+        case"play-3x":s.play(),
+          setTimeout(function(){
+            return s.player_.tech.el_.playbackRate=100
+          },100),
+          s.controlBar.fadeOut(),
+          $(".hero-actions").hide()
+      }
+      return false;
+    })
+  )
+});
